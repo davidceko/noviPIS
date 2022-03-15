c63f27372360454f3b4fafe1a0dea4d8f6b0e400
davidceko <cekodavid@gmail.com>
2022-03-15 17:04:39 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
